78f013d5a8fdc9827abfb408342b07ecc4a6e525
BaxSTAR <baxford03@gmail.com>
2023-07-21 10:06:57 +0800

    attempt 10

--- app/controllers/branch_controller.rb
+++ app/controllers/branch_controller.rb
@@ -10,7 +10,7 @@ class BranchController < ApplicationController
 
     def invite
         @user = params[:user]
-        @branchh = @user.branches.create(name: params[:name], user_id: params[:user_id])
+        @branchh = Branch.create(user: params[:user], name: params[:name], user_id: params[:user_id])
     end
 
     def new
