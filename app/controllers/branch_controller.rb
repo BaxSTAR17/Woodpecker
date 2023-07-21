d467955f98bd5c931ff768a37ca7a99445607b4b
BaxSTAR <baxford03@gmail.com>
2023-07-21 09:48:47 +0800

    attempt 9

--- app/controllers/branch_controller.rb
+++ app/controllers/branch_controller.rb
@@ -5,7 +5,7 @@ class BranchController < ApplicationController
 
     def create
         @peck = Current.user.pecks.create(post: params[:post], tree: params[:name], pecks: params[:pecks], user_id: Current.user.id)
-        redirect_to branched_path(:branchname => @branchname)
+        redirect_to branched_path(:branchname => params[:name])
     end
 
     def invite
