26b6a7b79bd918dd98eeb20941daf059d8d0d142
BaxSTAR <baxford03@gmail.com>
2023-07-22 13:22:05 +0800

    Please enter the commit message for your changes. Lines starting

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   post "login", to: "sessions#create"
 
   get "user", to: "account#index"
-  post "using", to: "account#create"
+  get "using", to: "account#create"
 
   get "another_user", to: "others#index"
 
